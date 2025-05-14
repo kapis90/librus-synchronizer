014748aa8e353f11c66b128123324788f3cc5b05
kapis <kapis90@gmail.com>
2025-05-14 22:11:52 +0200

    fix the workflow

--- create_token_pickle.py
+++ create_token_pickle.py
@@ -8,5 +8,11 @@ def create_token_pickle(destination: Path) -> None:
     """Create a token.pickle file with Google API credentials."""
     credentials = Credentials(
         token=os.getenv("G_TOKEN"),
+        refresh_token=os.getenv("G_REFRESH_TOKEN"),
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.getenv("G_CLIENT_ID"),
+        client_secret=os.getenv("G_CLIENT_SECRET"),
     )
+    # Ensure the destination directory exists
+    destination.parent.mkdir(parents=True, exist_ok=True)
     pickle.dump(credentials, destination.open("wb"))
