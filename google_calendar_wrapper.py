23c4f3b72c19a52a872eca04e71ecbfd6f516300
kapis <kapis90@gmail.com>
2025-05-14 22:22:58 +0200

    Do not create token.pickle

--- google_calendar_wrapper.py
+++ google_calendar_wrapper.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+import os
 from pathlib import Path
 from typing import Iterable
 
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
+from google.oauth2.credentials import Credentials
 
 from create_token_pickle import create_token_pickle
 
-TOKEN_PATH = Path(__file__).parent / ".credentials" / "token.pickle"
+# TOKEN_PATH = Path(__file__).parent / ".credentials" / "token.pickle"
+CREDENTIALS = Credentials(
+    token=os.getenv("G_TOKEN"),
+    refresh_token=os.getenv("G_REFRESH_TOKEN"),
+    token_uri="https://oauth2.googleapis.com/token",
+    client_id=os.getenv("G_CLIENT_ID"),
+    client_secret=os.getenv("G_CLIENT_SECRET"),
+)
+
 
 class GoogleCalendarWrapper:
     def __init__(self, email_address: str, calendar_id: str):
-        create_token_pickle(TOKEN_PATH)
-        self._google_calendar = GoogleCalendar(
-            email_address, token_path=str(TOKEN_PATH)
-        )
+        # create_token_pickle(TOKEN_PATH)
+        self._google_calendar = GoogleCalendar(email_address, credentials=CREDENTIALS)
         self._secondary_calendar = calendar_id
 
     def get_events(self, time_min: datetime) -> Iterable[Event]:
