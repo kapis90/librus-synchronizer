dcfe028bfa29ed0ed10d903c245520802325452a
kapis <kapis90@gmail.com>
2025-10-28 21:41:03 +0100

    Refactor token acquisition to include retry logic and update dependencies in pyproject.toml

--- execute.py
+++ execute.py
@@ -4,16 +4,23 @@ from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
 from librus_apix.client import Client, Token, new_client
+from retry import retry
+from urllib3.exceptions import MaxRetryError
 
 from google_calendar_wrapper import GoogleCalendarWrapper
 from librus_synchronizer import LibrusSynchronizer
 
 
-def main():
-    client: Client = new_client()
-    _token: Token = client.get_token(
+@retry(exceptions=MaxRetryError, tries=3, delay=2)
+def __aquire_librus_token(client: Client) -> Token:
+    """Aquire Librus token using environment variables LIBRUS_USERNAME and LIBRUS_PASSWORD."""
+    return client.get_token(
         str(os.getenv("LIBRUS_USERNAME")), str(os.getenv("LIBRUS_PASSWORD"))
     )
+
+def main():
+    client: Client = new_client()
+    _token: Token = __aquire_librus_token(client)
     calendar = GoogleCalendarWrapper(
         calendar_id=str(os.getenv("CALENDAR_ID")),
     )
